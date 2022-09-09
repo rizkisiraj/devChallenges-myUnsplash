07d7b99bf59ad3245ca4a970dc61ddd372b9f8de
rizkisiraj <jomirwa@gmail.com>
2022-09-09 22:10:37 +0700

    first commit

--- client/src/components/Photos Wrapper/photosWrapper.component.jsx
+++ client/src/components/Photos Wrapper/photosWrapper.component.jsx
@@ -1,12 +1,17 @@
-import dumpData from "../../dumpData";
+
+import { useContext } from "react";
+import { PictsContext } from "../../contexts/picts.context";
 import Photo from "../Photo/photo.component";
 
+
 const PhotosWrapper = () => {
+    const { filteredPicts } = useContext(PictsContext);
+
     return (
-        <main className="px-24 grid gap-11 grid-cols-gallery auto-rows-[250px] grid-flow-dense content-center justify-between">
+        <main className="px-8 md:px-16 lg:px-24 grid gap-11 grid-cols-1 md:grid-cols-gallery auto-rows-[250px] grid-flow-dense content-center justify-between sm:grid-cols-2">
             {
-                dumpData.map(pict => {
-                    return <Photo pict={pict} key={pict.id} />
+                filteredPicts && filteredPicts.map(pict => {
+                    return <Photo pict={pict} key={pict._id} />
                 })
             }
         </main>
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PictsProvider } from './contexts/picts.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <PictsProvider>
     <App />
+    </PictsProvider>
   </React.StrictMode>
 );
 
